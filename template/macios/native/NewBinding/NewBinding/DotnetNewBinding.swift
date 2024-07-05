51a306a930cc9ce09024a785a91c5d350efab0db
Merlin Eden <merlineden@MBP-von-Merlin.net>
2024-07-05 12:00:27 +0200

    Bind Usercentrics UI

--- template/macios/native/NewBinding/NewBinding/DotnetNewBinding.swift
+++ template/macios/native/NewBinding/NewBinding/DotnetNewBinding.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Usercentrics
+import UsercentricsUI
 
 @objc(DotnetNewBinding)
 public class DotnetNewBinding : NSObject
@@ -14,8 +15,26 @@ public class DotnetNewBinding : NSObject
 
     @objc
     public static func getString(myString: String) -> String {
-        UsercentricsOptions(settingsId: "")
-        return myString  + " from swift!"
+        UsercentricsCore.configure(options: UsercentricsOptions(settingsId: "Yf-hzQby0S9a7s"));
+        NSLog("Configured Usercentrics")
+        
+        UsercentricsCore.isReady { status in
+            NSLog("Usercentrics is ready")
+            
+            if status.shouldCollectConsent {
+                NSLog("Usercentrics should collect")
+                
+                UsercentricsBanner().showFirstLayer() { UsercentricsConsentUserResponse in
+                    NSLog("Usercentrics banned showed")
+                }
+            } else {
+                NSLog("Usercentrics should not collect consent")
+            }
+        } onFailure: { error in
+            NSLog("Usercentrics Something went wrong")
+        }
+        
+        return myString + " from Usercentrics"
     }
 
 }
